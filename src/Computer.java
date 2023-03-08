94b320758b6390866ea74c09e9323a3f61a8a182
liasf <lfaynberg8749@bths.edu>
2023-03-07 22:59:22 -0500

    Initial commit

---
+++ src/Computer.java
@@ -0,0 +1,12 @@
+public class Computer {
+    private int screenSize;
+    private int memory;
+
+    public Computer(int screenSize, int memory) {
+        this.screenSize = screenSize;
+        this.memory = memory;
+    }
+    public void installOperatingSystem() {
+        System.out.println("installing OS...");
+    }
+}
